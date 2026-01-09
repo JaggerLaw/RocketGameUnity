361d8f279552529b70ca615a3a3d9c081e07a5eb
Jake G <mamagotbig@gmail.com>
2026-01-09 16:42:37 +1300

    23.1 created new script, quits the game if q or esc is pressed

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuiteApplication : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame || Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            Application.Quit();
+        }
+    }
+}
